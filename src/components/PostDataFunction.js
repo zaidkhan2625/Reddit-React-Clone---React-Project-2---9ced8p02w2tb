7e49526f0e8c638adaa614a414aea882f456ef72
zaidkhan2625 <zaidkhan262523@gmail.com>
2024-03-11 18:18:06 +0530

    flight part done but hotel part is also done

--- src/components/PostDataFunction.js
+++ src/components/PostDataFunction.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "../styles/Result.css";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
@@ -8,48 +8,75 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 function PostDataFunction({
-    image,
-    name,
-    content,
-    likeCount,
-    commentCount,
-    PostImages,
-  }) {
-    return (
-      <>
-        <div className="resultdivsection">
-          <div className="resultHeadin">
-            <div className="rightheaddata">
-              <img className="profilelogo" src={image} />
-              <p className="username">{name}</p>
-              <p className="timePost">time of post</p>
-            </div>
-            <div className="joinbutton">
-              <button className="join">join</button>
-              <MoreHorizIcon className="horiz" />
-            </div>
+  image,
+  name,
+  content,
+  likeCount,
+  commentCount,
+  PostImages,
+}) {
+  return (
+    <>
+      <div className="resultdivsection">
+        <div className="resultHeadin">
+          <div className="rightheaddata">
+            <img className="profilelogo" src={image} />
+            <p className="username">{name}</p>
+            <p className="timePost">time of post</p>
           </div>
-          <div className="resultBody">
-            <p className="twoLineEllipsis">{content}</p>
-            <img className="PostImage" src={PostImages} />
-          </div>
-          <div className="resultFooter">
-            <div className="Like">
-              <ThumbUpOutlinedIcon /> <p className="likecounnt">{likeCount}</p>
-              <ThumbDownAltOutlinedIcon />
-            </div>
-            <div className="commentdiv">
-              <ChatBubbleOutlineOutlinedIcon />
-              <p className="comment">{commentCount}</p>
-            </div>
-            <div className="commentdiv">
-              <FontAwesomeIcon icon={faArrowUpFromBracket} />
-              <p className="share">share</p>
-            </div>
+          <div className="joinbutton">
+            <button className="join">join</button>
+            <MoreHorizIcon className="horiz" />
           </div>
         </div>
-      </>
-    );
-  }
+        <div className="resultBody">
+          <p className="twoLineEllipsis">{content}</p>
+          <img className="PostImage" src={PostImages} />
+        </div>
+        <div className="resultFooter">
+          <div className="Like">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-arrow-big-up"
+            >
+              <path d="M9 18v-6H5l7-7 7 7h-4v6H9z" />
+            </svg>
+            <p className="likecounnt">{likeCount}</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-arrow-big-down"
+            >
+              <path d="M15 6v6h4l-7 7-7-7h4V6h6z" />
+            </svg>
+          </div>
+          <div className="commentdiv">
+            <ChatBubbleOutlineOutlinedIcon />
+            <p className="comment">{commentCount}</p>
+          </div>
+          <div className="commentdiv">
+            <FontAwesomeIcon icon={faArrowUpFromBracket} />
+            <p className="share">share</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
-export default PostDataFunction
+export default PostDataFunction;
